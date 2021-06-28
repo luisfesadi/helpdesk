332e18f75a3fa84723bdb24a50c8df82490968d6
luisfesadi <luisfesadi@gmail.com>
2021-06-27 23:52:07 -0600

    gets completos y correcciones a las apis anteriores

--- routes/api/tickets/index.js
+++ routes/api/tickets/index.js
@@ -5,15 +5,21 @@ const {
   addNote, 
   captureTicket, 
   cerrar, 
-  evaluar
+  evaluar,
+  getTicketsByUser,
+  getTicketsByHolder,
+  getTickets,
+  getTicketsAge,
+  getTicketById
 } = require('./tickets.model');
 
 router.post(
   "/new",
   async (req, res)=>{
     try{
-      let {tipo, observacion, servicio, identidad, nombre, correo} = req.body;
-      let docInserted = await addNew(tipo, observacion, servicio, identidad, nombre, correo, 'ACT');
+      let {tipo, observacion, servicio, usuario} = req.body;
+      usuario = JSON.parse(usuario); // Convierte un String a un objeto JSON, el parametro viene escrito con la estructura de un JSON ejemplo {"identidad":"0827123412345", "nombre":"Scarlett Raquel","correo":"sraquel@gmail.com"}
+      let docInserted = await addNew(tipo, observacion, servicio, usuario, 'ACT');
       res.status(200).json(docInserted);
     }catch(ex){
       res.status(500).json({"msg":"Error"});
@@ -26,8 +32,9 @@ router.put(
   async (req, res)=>{
     try {
       const { id } = req.params;
-      const {observacion, accion, identidad, nombre, correo} = req.body;
-      let result = await addNote(id, observacion, accion, identidad, nombre, correo);
+      let {observacion, accion, user} = req.body;
+      user = JSON.parse(user); 
+      let result = await addNote(id, observacion, accion, user);
       res.status(200).json(result);
     } catch (ex) {
       res.status(500).json({"msg":"Error"});
@@ -40,8 +47,9 @@ router.put(
   async (req, res)=>{
     try {
       const { id } = req.params;
-      const { identidad, nombre, correo } = req.body;
-      let result = await captureTicket(id, identidad, nombre, correo);
+      let { user } = req.body;
+      user = JSON.parse(user);
+      let result = await captureTicket(id, user);
       res.status(200).json(result);
     } catch (ex) {
       res.status(500).json({"msg":"Error"});
@@ -54,8 +62,9 @@ router.put(
   async (req, res)=>{
     try {
       const { id } = req.params;
-      const { identidad, nombre, correo, tipoCierre} = req.body;
-      let result = await cerrar(id, identidad, nombre, correo, tipoCierre);
+      let { user, tipoCierre} = req.body;
+      user = JSON.parse(user);
+      let result = await cerrar(id, user, tipoCierre);
       res.status(200).json(result);
     } catch (ex) {
       res.status(500).json({"msg":"Error"});
@@ -73,6 +82,79 @@ router.put(
       res.status(200).json(result);
     } catch (ex) {
       res.status(500).json({"msg":"Error"});
+    }
+  }
+);
+
+router.get(
+  "/getbyuser/:estado/:page",
+  async (req, res)=>{
+    try {
+      let {estado, page} = req.params;
+      let {user} = req.body;
+      page = parseInt(page);
+      user = JSON.parse(user);
+      let result = await getTicketsByUser(user, estado, page);
+      res.status(200).json({...result, page, estado});
+    } catch (ex) {
+      res.status(500).json({ "msg": "Error" });
+    }
+  }
+);
+
+router.get(
+  "/getbyholder/:estado/:page",
+  async (req, res)=>{
+    try {
+      let {estado, page} = req.params;
+      let {holder} = req.body;
+      page = parseInt(page);
+      holder = JSON.parse(holder);
+      let result = await getTicketsByHolder(holder, estado, page);
+      res.status(200).json({...result, page, estado});
+    } catch (ex) {
+      res.status(500).json({ "msg": "Error" });
+    }
+  }
+);
+
+router.get(
+  "/gettickets/:estado/:page",
+  async (req, res)=>{
+    try {
+      let {estado, page} = req.params;
+      page = parseInt(page);
+      let result = await getTickets(estado, page);
+      res.status(200).json({...result, page, estado});
+    } catch (ex) {
+      res.status(500).json({ "msg": "Error" });
+    }
+  }
+);
+
+router.get(
+  "/getticketsage/:estado/:page",
+  async (req, res)=>{
+    try {
+      let {estado, page} = req.params;
+      page = parseInt(page);
+      let result = await getTicketsAge(estado, page);
+      res.status(200).json({...result, page, estado});
+    } catch (ex) {
+      res.status(500).json({ "msg": "Error" });
+    }
+  }
+);
+
+router.get(
+  "/getbyid/:id",
+  async (req, res)=>{
+    try {
+      let { id } = req.params;
+      let row = await getTicketById(id);
+      res.status(200).json(row);
+    } catch (ex) {
+      res.status(500).json({ "msg": "Error" });
     }
   }
 );
--- routes/api/tickets/tickets.model.js
+++ routes/api/tickets/tickets.model.js
@@ -18,7 +18,7 @@ let helpdeskCollection;
     }
 })();
 
-module.exports.addNew = async (tipo, observacion, servicio, identidad, nombre, correo, estado)=>{
+module.exports.addNew = async (tipo, observacion, servicio, usuario, estado)=>{
   try {
     let newTicket = {
       fecha: new Date().getTime(),
@@ -26,9 +26,9 @@ module.exports.addNew = async (tipo, observacion, servicio, identidad, nombre, c
       observacion: observacion,
       servicioAfectado: servicio,
       usuario: { 
-        identidad: identidad,
-        nombre: nombre,
-        correo: correo
+        identidad: usuario.identidad,
+        nombre: usuario.nombre,
+        correo: usuario.correo
       },
       holder: [],
       estado: estado,
@@ -46,7 +46,7 @@ module.exports.addNew = async (tipo, observacion, servicio, identidad, nombre, c
   }
 }
 
-module.exports.addNote = async (id, observacion, accion, identidad, nombre, correo) => {
+module.exports.addNote = async (id, observacion, accion, user) => {
   try {
     const fecha = new Date().getTime();
     const _id = new ObjectId(id);
@@ -59,9 +59,9 @@ module.exports.addNote = async (id, observacion, accion, identidad, nombre, corr
           "observacion": observacion,
           "accion": accion,
           "usuario": {
-            "identidad": identidad,
-            "nombre": nombre,
-            "correo": correo
+            "identidad": user.identidad,
+            "nombre": user.nombre,
+            "correo": user.correo
           }
         }
       } 
@@ -74,17 +74,17 @@ module.exports.addNote = async (id, observacion, accion, identidad, nombre, corr
   }
 }
 
-module.exports.captureTicket = async (id, identidad, nombre, correo) => {
+module.exports.captureTicket = async (id, user) => {
   try {
     const _id = new ObjectId(id);
     const filter = { "_id": _id };
     const updateObj = { 
-      "$push": //Duda en si es $set o $push, si se pueden almacenar varios o solo uno puede reclamarlo
+      "$set": //Duda en si es $set o $push, si se pueden almacenar varios o solo uno puede reclamarlo
       {
         "holder": {
-          "identidad": identidad,
-          "nombre": nombre,
-          "correo": correo
+          "identidad": user.identidad,
+          "nombre": user.nombre,
+          "correo": user.correo
         }
       }
     };
@@ -96,7 +96,7 @@ module.exports.captureTicket = async (id, identidad, nombre, correo) => {
   }
 }
 
-module.exports.cerrar = async (id, identidad, nombre, correo, tipoCierre) => {
+module.exports.cerrar = async (id, user, tipoCierre) => {
   try {
     const fecha = new Date().getTime();
     const _id = new ObjectId(id);
@@ -106,9 +106,9 @@ module.exports.cerrar = async (id, identidad, nombre, correo, tipoCierre) => {
       {
         "fechaCierre": fecha,
         "usuarioCierre": {
-          "identidad": identidad,
-          "nombre": nombre,
-          "correo": correo
+          "identidad": user.identidad,
+          "nombre": user.nombre,
+          "correo": user.correo
         },
         "tipoCierre": tipoCierre
       }
@@ -138,6 +138,154 @@ module.exports.evaluar = async (id, eficiencia, satisfaccion, conformidad) => {
     };
     let result = await helpdeskCollection.updateOne(filter, updateObj);
     return result;
+  } catch (ex) {
+    console.log(ex);
+    throw(ex);
+  }
+}
+
+let itemsPerPage = 25; //Variable global para la cantidad de registros por pagina
+
+module.exports.getTicketsByUser = async (user, estado, page) => {
+  try {
+    let options = {
+      skip: (page - 1) * itemsPerPage,
+      limit: itemsPerPage,
+      projection: {
+        fecha:1, 
+        tipo:1, 
+        observacion:1,
+        servicioAfectado:1,
+        usuario:1,
+        holder:1,
+        notas:1
+      }, //Los campos a mostrar
+      sort: [["fecha", -1]] //muestra los registros por fecha descendente
+    };
+    console.log(user.identidad);
+    let filter = { 
+      "usuario": {
+        "identidad": user.identidad,
+        "nombre": user.nombre,
+        "correo": user.correo
+      }, 
+      "estado": estado 
+    };
+
+    let docsCursor = helpdeskCollection.find(filter, options);
+    let rownum = await docsCursor.count();
+    let rows = await docsCursor.toArray();
+    return {rownum, rows};
+  } catch (ex) {
+    console.log(ex);
+    throw(ex);
+  }
+}
+
+module.exports.getTicketsByHolder = async (user, estado, page) => {
+  try {
+    let options = {
+      skip: (page - 1) * itemsPerPage,
+      limit: itemsPerPage,
+      projection: {
+        fecha:1, 
+        tipo:1, 
+        observacion:1,
+        servicioAfectado:1,
+        usuario:1,
+        holder:1,
+        notas:1
+      }, //Los campos a mostrar
+      sort: [["fecha", -1]] //muestra los registros por fecha descendente
+    };
+    console.log(user.identidad);
+    let filter = { 
+      "holder": {
+        "identidad": user.identidad,
+        "nombre": user.nombre,
+        "correo": user.correo
+      }, 
+      "estado": estado 
+    };
+
+    let docsCursor = helpdeskCollection.find(filter, options);
+    let rownum = await docsCursor.count();
+    let rows = await docsCursor.toArray();
+    return {rownum, rows};
+  } catch (ex) {
+    console.log(ex);
+    throw(ex);
+  }
+}
+
+module.exports.getTickets = async (estado, page) => {
+  try {
+    let options = {
+      skip: (page - 1) * itemsPerPage,
+      limit: itemsPerPage,
+      projection: {
+        fecha:1, 
+        tipo:1, 
+        observacion:1,
+        servicioAfectado:1,
+        usuario:1,
+        holder:1,
+        notas:1
+      }, //Los campos a mostrar
+      sort: [["fecha", -1]] //muestra los registros por fecha descendente
+    };
+    
+    let filter = { 
+      "estado": estado
+    };
+
+    let docsCursor = helpdeskCollection.find(filter, options);
+    let rownum = await docsCursor.count();
+    let rows = await docsCursor.toArray();
+    return {rownum, rows};
+  } catch (ex) {
+    console.log(ex);
+    throw(ex);
+  }
+}
+
+module.exports.getTicketsAge = async (estado, page) => {
+  try {
+    let options = {
+      skip: (page - 1) * itemsPerPage,
+      limit: itemsPerPage,
+      projection: {
+        fecha:1, 
+        tipo:1, 
+        observacion:1,
+        servicioAfectado:1,
+        usuario:1,
+        holder:1,
+        notas:1
+      }, //Los campos a mostrar
+      sort: [["fecha", -1]] //muestra los registros por fecha descendente
+    };
+
+    let filter = {
+      "estado":estado 
+    };
+
+    let docsCursor = helpdeskCollection.find(filter, options);
+    let rownum = await docsCursor.count();
+    let rows = await docsCursor.toArray();
+    return {rownum, rows};
+  } catch (ex) {
+    console.log(ex);
+    throw(ex);
+  }
+}
+
+module.exports.getTicketById = async (id) => {
+  try {
+    const _id = new ObjectId(id);
+    const filter = { "_id": _id };
+    let row = helpdeskCollection.findOne(filter);
+    return row;
   } catch (ex) {
     console.log(ex);
     throw(ex);

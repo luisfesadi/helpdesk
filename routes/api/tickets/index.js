94fb289e4f88c50e1ea8c0e36e20feb7dbc71296
luisfesadi <luisfesadi@gmail.com>
2021-06-27 22:06:47 -0600

    apis cerrar, evaluar y capturar ticket listas

--- routes/api/tickets/index.js
+++ routes/api/tickets/index.js
@@ -1,32 +1,80 @@
 const express = require('express');
 const router = express.Router();
-const { addNew, addNote } = require('./tickets.model');
+const { 
+  addNew, 
+  addNote, 
+  captureTicket, 
+  cerrar, 
+  evaluar
+} = require('./tickets.model');
 
 router.post(
-    "/new",
-    async (req, res)=>{
-      try{
-        let {tipo, observacion, servicio, identidad, nombre, correo} = req.body;
-        let docInserted = await addNew(tipo, observacion, servicio, identidad, nombre, correo, 'Activo');
-        res.status(200).json(docInserted);
-      }catch(ex){
-        res.status(500).json({"msg":"Error"});
-      }
+  "/new",
+  async (req, res)=>{
+    try{
+      let {tipo, observacion, servicio, identidad, nombre, correo} = req.body;
+      let docInserted = await addNew(tipo, observacion, servicio, identidad, nombre, correo, 'ACT');
+      res.status(200).json(docInserted);
+    }catch(ex){
+      res.status(500).json({"msg":"Error"});
     }
+  }
 );
 
 router.put(
-    "/addnote/:id",
-    async (req, res)=>{
-        try {
-            const { id } = req.params;
-            const {observacion, accion, identidad, nombre, correo} = req.body;
-            let result = await addNote(id, observacion, accion, identidad, nombre, correo);
-            res.status(200).json(result);
-        } catch (ex) {
-            res.status(500).json({"msg":"Error"});
-        }
+  "/addnote/:id",
+  async (req, res)=>{
+    try {
+      const { id } = req.params;
+      const {observacion, accion, identidad, nombre, correo} = req.body;
+      let result = await addNote(id, observacion, accion, identidad, nombre, correo);
+      res.status(200).json(result);
+    } catch (ex) {
+      res.status(500).json({"msg":"Error"});
     }
+  }
+);
+
+router.put(
+  "/captureticket/:id",
+  async (req, res)=>{
+    try {
+      const { id } = req.params;
+      const { identidad, nombre, correo } = req.body;
+      let result = await captureTicket(id, identidad, nombre, correo);
+      res.status(200).json(result);
+    } catch (ex) {
+      res.status(500).json({"msg":"Error"});
+    }
+  }
+);
+
+router.put(
+  "/cerrar/:id",
+  async (req, res)=>{
+    try {
+      const { id } = req.params;
+      const { identidad, nombre, correo, tipoCierre} = req.body;
+      let result = await cerrar(id, identidad, nombre, correo, tipoCierre);
+      res.status(200).json(result);
+    } catch (ex) {
+      res.status(500).json({"msg":"Error"});
+    }
+  }
+);
+
+router.put(
+  "/evaluar/:id",
+  async (req, res)=>{
+    try {
+      const { id } = req.params;
+      const { eficiencia, satisfaccion, conformidad} = req.body;
+      let result = await evaluar(id, eficiencia, satisfaccion, conformidad);
+      res.status(200).json(result);
+    } catch (ex) {
+      res.status(500).json({"msg":"Error"});
+    }
+  }
 );
 
 module.exports = router;
--- routes/api/tickets/tickets.model.js
+++ routes/api/tickets/tickets.model.js
@@ -68,14 +68,76 @@ module.exports.addNote = async (id, observacion, accion, identidad, nombre, corr
     };
     let result = await helpdeskCollection.updateOne(filter, updateObj);
     return result;
-    /*fecha:
-      observacion:
-      accion:
-      usuario: {
-        identidad:
-        nombre:
-        correo:
-      }*/
+  } catch (ex) {
+    console.log(ex);
+    throw(ex);
+  }
+}
+
+module.exports.captureTicket = async (id, identidad, nombre, correo) => {
+  try {
+    const _id = new ObjectId(id);
+    const filter = { "_id": _id };
+    const updateObj = { 
+      "$push": //Duda en si es $set o $push, si se pueden almacenar varios o solo uno puede reclamarlo
+      {
+        "holder": {
+          "identidad": identidad,
+          "nombre": nombre,
+          "correo": correo
+        }
+      }
+    };
+    let result = await helpdeskCollection.updateOne(filter, updateObj);
+    return result;
+  } catch (ex) {
+    console.log(ex);
+    throw(ex);
+  }
+}
+
+module.exports.cerrar = async (id, identidad, nombre, correo, tipoCierre) => {
+  try {
+    const fecha = new Date().getTime();
+    const _id = new ObjectId(id);
+    const filter = { "_id": _id };
+    const updateObj = { 
+      "$set": 
+      {
+        "fechaCierre": fecha,
+        "usuarioCierre": {
+          "identidad": identidad,
+          "nombre": nombre,
+          "correo": correo
+        },
+        "tipoCierre": tipoCierre
+      }
+    };
+    let result = await helpdeskCollection.updateOne(filter, updateObj);
+    return result;
+  } catch (ex) {
+    console.log(ex);
+    throw(ex);
+  }
+}
+
+module.exports.evaluar = async (id, eficiencia, satisfaccion, conformidad) => {
+  try {
+    const _id = new ObjectId(id);
+    const filter = { "_id": _id };
+    const updateObj = {
+       "$set":
+       {
+         "evaluacion":
+         {
+           "eficiencia": eficiencia,
+           "satisfaccion": satisfaccion,
+           "conformidad": conformidad
+         }
+       }  
+    };
+    let result = await helpdeskCollection.updateOne(filter, updateObj);
+    return result;
   } catch (ex) {
     console.log(ex);
     throw(ex);
